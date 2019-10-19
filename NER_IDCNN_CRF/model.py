aa8c700ad6added9556ba20cb062254d84b50864
carlos9310 <15356059310@163.com>
2019-10-19 20:03:37 +0800

    update

--- NER_IDCNN_CRF/model.py
+++ NER_IDCNN_CRF/model.py
@@ -393,7 +393,7 @@ class Model(object):
 
     def evaluate_line(self, sess, inputs, id_to_tag):
         trans = self.trans.eval()
-        lengths, scores = self.run_step(sess, False, inputs)
+        lengths, scores, _ = self.run_step(sess, False, inputs)
         batch_paths = self.decode(scores, lengths, trans)
         tags = [id_to_tag[idx] for idx in batch_paths[0]]
         return result_to_json(inputs[0][0], tags)
